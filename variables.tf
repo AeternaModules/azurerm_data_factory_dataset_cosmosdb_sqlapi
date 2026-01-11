b24346deb0c44ea7bb2c006f532c922bfbb24ec5
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:42:34 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "data_factory_dataset_cosmosdb_sqlapis" {
+  description = <<EOT
+Map of data_factory_dataset_cosmosdb_sqlapis, attributes below
+Required:
+    - data_factory_id
+    - linked_service_name
+    - name
+Optional:
+    - additional_properties
+    - annotations
+    - collection_name
+    - description
+    - folder
+    - parameters
+    - schema_column (block):
+        - description (optional)
+        - name (required)
+        - type (optional)
+EOT
+
+  type = map(object({
+    data_factory_id       = string
+    linked_service_name   = string
+    name                  = string
+    additional_properties = optional(map(string))
+    annotations           = optional(list(string))
+    collection_name       = optional(string)
+    description           = optional(string)
+    folder                = optional(string)
+    parameters            = optional(map(string))
+    schema_column = optional(object({
+      description = optional(string)
+      name        = string
+      type        = optional(string)
+    }))
+  }))
+}
+
